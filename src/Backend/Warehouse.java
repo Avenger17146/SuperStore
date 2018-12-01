eab9114bece7ee7a36b2acb5296dd8a9b28866fb
Avenger <dhruv17146@iiitd.ac.in>
2018-12-01 16:18:27 +0530

    Database update

--- src/Backend/Warehouse.java
+++ src/Backend/Warehouse.java
@@ -1,4 +1,49 @@
 package Backend;
 
+
+
+import java.util.ArrayList;
+
 public class Warehouse {
+    private Database db;
+    private String Name;
+    private int ID;
+    private ArrayList<Message> messages;
+
+    public Database getDb() {
+        return db;
+    }
+
+    public void setDb(Database db) {
+        this.db = db;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public ArrayList<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(ArrayList<Message> messages) {
+        this.messages = messages;
+    }
+
+    public Warehouse(String name, int ID) {
+        Name = name;
+        this.ID = ID;
+    }
 }
